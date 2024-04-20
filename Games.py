8087725db6423793a4550e48c403d9aad9a6d39d
199No <Nobear08@outlook.com>
2024-04-20 09:20:39 -0700

    addedApple

--- Games.py
+++ Games.py
@@ -6,18 +6,20 @@ background = Rect(0,0,400,400,fill='darkgreen')
 screen = Rect(20,10,360,40,fill='white',border='darkgrey')
 title = Label('Snake Game', 200,30,size=25)
 
-
-keyLabel = Label('', 200, 200, size=50)
-
+apple = Circle(200, 200, 10, fill='Red', borderWidth=5)
 
 cmu_graphics.run()
 
 def onKeyPress(key):
-    keyLabel.value = key
-    print(keyLabel)
-
-
-def onKeyPress(key):
-    # Move the sword and brighten the sky and gem in Excalibur's handle.
-    print('hhhhhh')
-    pass
+    if(key == 'w'):
+        #move up
+        print('w')
+    elif(key == 'a'):
+        #move left
+        print('a')
+    elif(key == 's'):
+        #move down
+        print('s')
+    elif(key == 'd'):
+        #move right
+        print('d')
