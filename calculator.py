9b7b4cf73983ffbc4c3fe88b30b1712b579014f4
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 08:51:27 -0700

    calculator start

---
+++ calculator.py
@@ -0,0 +1,8 @@
+from cmu_graphics import *
+import mu_graphics
+
+background = Rect(0,0,400,400,fill='lightgrey')
+title = Label('Calulator App', 200,20,size=25)
+screen = Rect(20,40,360,60,fill='white',border='darkgrey')
+
+cmu_graphics.run()
