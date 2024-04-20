d8150500811f312aed4cd57a443f912c6c16042d
199No <Nobear08@outlook.com>
2024-04-20 09:04:40 -0700

    Game stuff

--- Games.py
+++ Games.py
@@ -1,9 +1,12 @@
+##Importing 
 from cmu_graphics import *
 
 
 background = Rect(0,0,400,400,fill='darkgreen')
 screen = Rect(20,10,360,40,fill='white',border='darkgrey')
 title = Label('Snake Game', 200,30,size=25)
+
+
 
 
 cmu_graphics.run()
