821221196dd3e6bf7701cb92449e86d52f0dcb5d
199No <Nobear08@outlook.com>
2024-04-20 08:28:11 -0700

    Fixed Spacing

--- cmu.py
+++ cmu.py
@@ -3,7 +3,7 @@ from cmu_graphics import *
 sky = Rect(0, 0, 400, 400, fill='midnightBlue')
 
 ground = Rect(0, 350, 400, 85,
-              fill=gradient(rgb(0, 60, 0), rgb(0, 80, 0), start='bottom'))
+            fill=gradient(rgb(0, 60, 0), rgb(0, 80, 0), start='bottom'))
 
 excalibur = Group(
     Polygon(185, 150, 215, 150, 215, 325, 200, 365, 185, 325,
