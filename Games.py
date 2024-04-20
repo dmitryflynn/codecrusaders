d568042a985f7488fd4af7dcdf2a8199a740f94a
199No <Nobear08@outlook.com>
2024-04-20 08:56:00 -0700

    Added Basic Screen

---
+++ Games.py
@@ -0,0 +1,9 @@
+from cmu_graphics import *
+
+
+background = Rect(0,0,400,400,fill='darkgreen')
+screen = Rect(20,10,360,40,fill='white',border='darkgrey')
+title = Label('Snake Game', 200,30,size=25)
+
+
+cmu_graphics.run()
