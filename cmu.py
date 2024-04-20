584580ae513f89feb289cbe60e8f29735286ad21
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 07:48:18 -0700

    centerX

--- cmu.py
+++ cmu.py
@@ -2,9 +2,9 @@
 from cmu_graphics import *
 
 Rect(0,0,400,400,fill='skyblue')
-Circle(0,200,4,fill='green')
+circle = Circle(0,200,4,fill='green')
 
 for i in range(400):
-    Circle.centerX += 1
+     circle.centerX += 1
 
 cmu_graphics.run()  
