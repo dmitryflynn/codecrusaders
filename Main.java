ad9215f20afc1dfda5f63869a0655312ba5406a4
199No <Nobear08@outlook.com>
2024-04-20 07:24:43 -0700

    I Added User Input

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
-public main{
-    //This is a test
+
+//Imports
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Start print statement
+        System.out.println("LET'S DO THIS!");
+        
+        //Making The Scanner
+        Scanner scanner = new Scanner(System.in);
+        String LInput = scanner.nextString();
+        System.out.println(LInput);
+    }
 }
