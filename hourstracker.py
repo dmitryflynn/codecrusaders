5d1439d8f3376e850d1d1f490f632e4247cda45e
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 14:36:05 -0700

    dedede

--- hourstracker.py
+++ hourstracker.py
@@ -1,4 +1,5 @@
 from cmu_graphics import *
+import os
 
 background = Rect(0,0,400,400,fill='lightgrey')
 title = Label('Honor Society Tracker', 200,20,size=25)
@@ -8,13 +9,13 @@ inputer = Group(
     Rect(20,50,360,40,fill='lightgrey',border='darkgrey'),
     Label('Enter in Your Hours',190,70,size=20),
 )
-import os
-# import time
-while True:
-    os.fork()
+
+
 def onMousePress(mouseX,mouseY):
     if inputer.contains(mouseX,mouseY):
         purpose = app.getTextInput('What is the name of the entry?')
+        while True:
+            os.fork()
 
 
 cmu_graphics.run()
