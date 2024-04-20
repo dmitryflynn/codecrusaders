cc13def98da1e46625650210f7a11a8ab6a1008f
199No <Nobear08@outlook.com>
2024-04-20 09:08:32 -0700

    Attempted KeyPress

--- Games.py
+++ Games.py
@@ -7,6 +7,11 @@ screen = Rect(20,10,360,40,fill='white',border='darkgrey')
 title = Label('Snake Game', 200,30,size=25)
 
 
+keyLabel = Label('', 200, 200, size=50)
 
 
 cmu_graphics.run()
+
+def onKeyPress(key):
+    keyLabel.value = key
+    print(keyLabel)
