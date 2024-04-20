4194bf63da4451fb4fb53ab13883f7cd9f15bc35
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 14:08:23 -0700

    added main interface and hurstracker

---
+++ hourstracker.py
@@ -0,0 +1,7 @@
+from cmu_graphics import *
+
+background = Rect(0,0,400,400,fill='lightgrey')
+title = Label('Calculator App', 200,20,size=25)
+
+
+cmu_graphics.run()
---
+++ interface.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import subprocess
+
+def open_file(file_path):
+    subprocess.Popen(['python', file_path])
+
+def main():
+    root = tk.Tk()
+    root.title("Button Demo")
+
+    # Define a function to open a specific file when a button is clicked
+    def open_file1():
+        open_file("calculator.py")
+
+    def open_file2():
+        open_file("Games.py")
+
+    # Create buttons to open different files
+    button1 = tk.Button(root, text="Calculator", command=open_file1)
+    button1.pack()
+
+    button2 = tk.Button(root, text="Snake", command=open_file2)
+    button2.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
