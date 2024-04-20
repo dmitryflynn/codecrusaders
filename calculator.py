9ccf8986e5adbb449e6498ab4475abb3782c7f53
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 10:14:10 -0700

    implemented addition

--- calculator.py
+++ calculator.py
@@ -1,68 +1,83 @@
 from cmu_graphics import *
-import cmu_graphics
+
 
 background = Rect(0,0,400,400,fill='lightgrey')
 title = Label('Calulator App', 200,20,size=25)
 screen = Rect(20,40,360,60,fill='white',border='darkgrey')
 
-button1Group = Group(
-    button1 = Rect(20,120,40,40,fill='gray'),
-    button1Label = Label('1',40,140,fill='white',size=26),
-    )
+button1 = Rect(20,120,40,40,fill='gray')
+button1.value = 1
+button1Label = Label('1',40,140,fill='white',size=26)
 
-button2Group = Group(
-    button2 = Rect(80,120,40,40,fill='gray'),
-    button2Label = Label('2',100,140,fill='white',size=26),
-)
+button2 = Rect(80,120,40,40,fill='gray')
+button2.value = 2
+button2Label = Label('2',100,140,fill='white',size=26)
 
-button3Group = Group(
-    button3 = Rect(140,120,40,40,fill='gray'),
-    button3Label = Label('3',160,140,fill='white',size=26),
-    )
+button3 = Rect(140,120,40,40,fill='gray')
+button3.value = 3
+button3Label = Label('3',160,140,fill='white',size=26)
 
+button4 = Rect(20,180,40,40,fill='gray')
+button4.value = 4
+button4Label = Label('4',40,200,fill='white',size=26)
 
-button4Group = Group(
-    button4 = Rect(20,180,40,40,fill='gray'),
-    button4Label = Label('4',40,200,fill='white',size=26),
-    )
+button5 = Rect(80,180,40,40,fill='gray')
+button5.value = 5
+button5Label = Label('5',100,200,fill='white',size=26)
 
-button5Group = Group(
-    button5 = Rect(80,180,40,40,fill='gray'),
-    button5Label = Label('5',100,200,fill='white',size=26),
-    )
+button6 = Rect(140,180,40,40,fill='gray')
+button6.value = 6
+button6Label = Label('6',160,200,fill='white',size=26)
 
-button6Group = Group(
-    button6 = Rect(140,180,40,40,fill='gray'),
-    button6Label = Label('6',160,200,fill='white',size=26),
-    )
+button7 = Rect(20,240,40,40,fill='gray')
+button7.value = 7
+button7Label = Label('7',40,260,fill='white',size=26)
 
-button7Group = Group(
-    button7 = Rect(20,240,40,40,fill='gray'),
-    button7Label = Label('7',40,260,fill='white',size=26),
-    )
+button8 = Rect(80,240,40,40,fill='gray')
+button8.value = 8
+button8Label = Label('8',100,260,fill='white',size=26)
 
-button8Group = Group(
-    button8 = Rect(80,240,40,40,fill='gray'),
-    button8Label = Label('8',100,260,fill='white',size=26),
-    )
+button9 = Rect(140,240,40,40,fill='gray')
+button9.value = 9
+button9Label = Label('9',160,260,fill='white',size=26)
 
-button9Group = Group(
-    button9 = Rect(140,240,40,40,fill='gray'),
-    button9Label = Label('9',160,260,fill='white',size=26),
-    )
+addition = Rect(340,120,40,40,fill='gray')
+additonLabel = Label('+',360,140,fill='white',size=26)
 
-button1Group.value = 1
-button2Group.value = 2
-button3Group.value = 3
-button4Group.value = 4
-button5Group.value = 5
-button6Group.value = 6
-button7Group.value = 7
-button8Group.value = 8
-button9Group.value = 9
+equals = Rect(340,300,40,40,fill='gray')
+equalsLabel = Label('=',360,320,fill='white',size=26)
 
-def keyChecker(mouseX,mouseY):
-    pass
+operator = 'plus'
+numbers = [button1,button2,button3,button4,button5,button6,button7,button8,button9]
+current_numbers = []
+app.currentnumber = ''
+app.newnumber = 0
+result = Label(app.currentnumber,200,200,fill='white',size=30)
+
+def keyChecker(X, Y):
+    global operator
+    for i in range(len(numbers)):
+        if numbers[i].contains(X, Y):
+
+            current_numbers.append(numbers[i].value)
+            app.currentnumber = ''
+            for k in range(len(current_numbers)):
+                app.currentnumber += str(current_numbers[k])
+                
+            result.value = app.currentnumber
     
+def onMousePress(mouseX,mouseY):
+    global operator
+    if addition.contains(mouseX, mouseY):
+        operator = 'plus'
+        app.newnumber = int(app.currentnumber)
+        app.currentnumber = ''
+        result.value = ''
+    elif equals.contains(mouseX,mouseY):
+        if operator == 'plus':
+            total = app.newnumber + int(app.currentnumber)
+            result.value = str(total)
+    else:
+        keyChecker(mouseX, mouseY)
 
 cmu_graphics.run()
