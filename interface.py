776c25833695b4f6b3c8daf5eab414f361a2da5c
dmitryflynn <dmitryflynn@outlook.com>
2024-04-20 18:49:34 -0700

    updated menu

--- interface.py
+++ interface.py
@@ -1,15 +1,16 @@
-import tkinter as tk
-import subprocess
+import tkinter as tk #Import tkinter under alias of tk
+import subprocess #Import subproccess to allow you to open processes
 
-def open_file(file_path):
-    subprocess.Popen(['python', file_path])
+def open_file(file_path):  #create a function that takes a filepath as a pararemeter
+    subprocess.Popen(['python', file_path]) #use default python process to run files
 
 def main():
-    root = tk.Tk()
-    root.title("Button Demo")
+    root = tk.Tk() #New instance of main window
+    root.title("Student ")
     root.configure(bg='#ADD8E6')  # Set background color to light blue
 
     # Define a function to open a specific file when a button is clicked
+    .
     def open_file1():
         open_file("calculator.py")
 
@@ -29,7 +30,7 @@ def main():
     button3 = tk.Button(root, text="Hours Tracker", command=open_file3, bg='#ADD8E6', fg='white', relief=tk.FLAT, activebackground='#61cefe', activeforeground='white', borderwidth=0, highlightthickness=0)
     button3.pack(pady=10)
 
-    root.mainloop()
+    root.mainloop() #Starts event loop
 
 if __name__ == "__main__":
     main()
